269b4ba9c95c6f3aff38c009a428e7d965c00388
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-05-22 00:10:33 +0530

    Removed CORS headers

--- index.ts
+++ index.ts
@@ -21,14 +21,6 @@ admin.initializeApp({
 app.use(express.json())
 app.use(express.urlencoded({extended : true}));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
-    next();
-  }
-);
-
 app.use(authenticateToken);
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
